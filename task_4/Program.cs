fdc151e2cd1970a6964c8a67e645765928ae4568
Cat <ligeia_undead@mail.ru>
2023-02-04 02:14:23 +0300

    task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+int a = 2;
+int b = 3395;
+int c = 7;
+
+int max = a;
+
+if (a>max)max = a;
+if (b>max)max = b;
+if (c>max)max = c;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+
