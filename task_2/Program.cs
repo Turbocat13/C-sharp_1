9cb98459f4839fcbce3ba8a0308ea53f1d056915
Cat <ligeia_undead@mail.ru>
2023-02-04 01:56:00 +0300

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+int a = 25;
+int b = 7;
+
+int max = a;
+int min = b;
+
+if (b>max) max = b; 
+else max = a;
+
+if (b<max) min = b;
+else min = a;
+
+              
+Console.Write("max= ");
+Console.WriteLine(max);
+
+Console.Write("min= ");
+Console.WriteLine(min);
